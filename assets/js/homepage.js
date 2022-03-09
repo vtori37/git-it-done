9c24c431cf1842abbeeea48c52c5d5c45c5de85e
Tori Acer <vrice360@gmail.com>
2022-03-08 18:26:11 -0600

    fetch octocat repo

---
+++ assets/js/homepage.js
@@ -0,0 +1,12 @@
+var getUserRepos = function() {
+  fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+  response.json().then(function(data) {
+    console.log(data);
+  });
+});
+
+console.log("outside");
+
+};
+
+getUserRepos();
