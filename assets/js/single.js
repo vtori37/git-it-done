1d9c39651e2cfdc18b57990c708840d8198b0c56
Tori Acer <vrice360@gmail.com>
2022-03-10 18:13:40 -0600

    see list of more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,7 +1,8 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
-  console.log(repo);
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
   fetch(apiUrl).then(function(response) {
@@ -10,9 +11,15 @@ var getRepoIssues = function(repo) {
       response.json().then(function(data) {
         // pass response data to dom function
         displayIssues(data);
+      
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
       });
     }
     else {
+      console.log(response);
       alert("There was a problem with your request!");
     }
   });
@@ -53,5 +60,18 @@ var displayIssues = function(issues) {
   // append to container
   issueEl.appendChild(typeEl);
   issueContainerEl.appendChild(issueEl);
-}
+  }
+
+};
+
+var displayWarning = function(repo) {
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+  
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
 };
